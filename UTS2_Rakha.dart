01494ba507f062b66bcfc1127d6e5018ad2d359c
Rakha Abyn Nugroho <202010225101@mhs.ubharajaya.ac.id>
2022-11-01 21:53:04 +0700

    UTS Rakha
    202010225101

---
+++ UTS2_Rakha.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  print ("* Program Input Dan Percabangan If *");
+
+ stdout.write("Nama         : ");
+ String? Nama = stdin.readLineSync();
+
+ stdout.write("Nilai UTS    : ");
+ int uts = int.parse(stdin.readLineSync()!);
+
+ stdout.write("Kelas        : ");
+ dynamic Kelas = stdin.readLineSync();
+
+ stdout.write("Mata Kuliah  : ");
+ String? Matkul = stdin.readLineSync();
+
+ String grade;
+
+ 
+ if (uts >= 90 - 85)
+ grade = "A";
+ else if (uts >= 84 - 75)
+ grade = "B";
+ else if (uts >= 74 - 65)
+ grade = "C";
+ else if (uts >= 64 - 50)
+ grade = "D";
+ else if (uts <= 50)
+ grade = "E";
+ else; 
+
+  
+
+}
